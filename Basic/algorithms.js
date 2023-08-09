f280c87d605395a9be3520c368ebf8f717aa635f
facundoacostayl <facundoacostayl@outlook.com>
2023-08-09 05:53:43 -0300

    Add Factorialize a Number

--- Basic/algorithms.js
+++ Basic/algorithms.js
@@ -11,3 +11,15 @@ function reverseString(str) {
 }
 
 reverseString("hello");
+
+// 3 - FACTORIALIZE A NUMBER
+function factorialize(num) {
+  let newNum = 1;
+  for (let i = 1; i <= num; i++) {
+    newNum *= i;
+  }
+
+  return newNum;
+}
+
+factorialize(5);
