7090f5b8887e91595edd2c71dc676f1117db2cb5
facundoacostayl <facundoacostayl@outlook.com>
2023-08-12 06:12:54 -0300

    Add Truncate a String

--- Basic/algorithms.js
+++ Basic/algorithms.js
@@ -90,3 +90,10 @@ function repeatStringNumTimes(str, num) {
 }
 
 repeatStringNumTimes("abc", 3);
+
+// 8 - TRUNCATE A STRING
+function truncateString(str, num) {
+  return str.length <= num ? str : str.substring(0, num) + "...";
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
