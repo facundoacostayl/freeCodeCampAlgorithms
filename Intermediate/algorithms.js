72a46c546d99d1f06f3d492e4cfac52116f3ec99
facundoacostayl <facundoacostayl@outlook.com>
2023-08-26 05:17:52 -0300

    Add Diff Two Arrays

--- Intermediate/algorithms.js
+++ Intermediate/algorithms.js
@@ -12,3 +12,22 @@ function sumAll(arr) {
 }
 
 sumAll([1, 4]);
+
+// 2 - DIFF TWO ARRAYS
+function diffArray(arr1, arr2) {
+  const newArr = [];
+  const longestArr = arr1.length > arr2.length ? arr1 : arr2;
+  const shortestArr = arr1 == longestArr ? arr2 : arr1;
+
+  for (let i = 0; i < longestArr.length; i++) {
+    !shortestArr.includes(longestArr[i]) && newArr.push(longestArr[i]);
+  }
+
+  for (let i = 0; i < shortestArr.length; i++) {
+    !longestArr.includes(shortestArr[i]) && newArr.push(shortestArr[i]);
+  }
+
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
